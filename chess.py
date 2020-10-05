35fabf4a7a52577b4eeedfd55341ac1a610393ec
CI0N3 <63483127+CI0N3@users.noreply.github.com>
2020-10-04 21:14:04 -0500

    Update chess.py

--- chess.py
+++ chess.py
@@ -27,15 +27,6 @@ def print_board(board):
 
 
 def initial_state():
-    return [[B_R, E, E, E, B_K, E, E, B_R],
-            [B_P, E, E, E, E, E, E, B_P],
-            [E, E, E, E, E, E, E, E],
-            [E, E, E, E, E, E, E, E],
-            [E, E, E, E, E, E, E, E],
-            [E, E, E, E, E, E, E, E],
-            [E, E, E, E, E, E, E, E],
-            [W_R, E, E, E, W_K, E, E, W_R]]
-    """""
     return [[B_R, B_N, B_B, B_Q, B_K, B_B, B_N, B_R],
             [B_P, B_P, B_P, B_P, B_P, B_P, B_P, B_P],
             [E, E, E, E, E, E, E, E],
@@ -44,7 +35,6 @@ def initial_state():
             [E, E, E, E, E, E, E, E],
             [W_P, W_P, W_P, W_P, W_P, E, W_P, W_P],
             [W_R, E, E, E, W_K, E, E, W_R]]
-    """""
 
 
 def check(board, player):
@@ -439,8 +429,6 @@ def utility(board):
             if board[i][j] == B_Q:
                 B_Points += 9
     # Returns a positive number if white is winning, negative number if black is winning in terms of pieces
-    #print(W_Points)
-    #print(B_Points)
     return W_Points - B_Points
 
 
